ca6d0e3fd997e2e8ae72fb4b2bec53256c07d154
Aleksei Usov <usalekse@gmail.com>
2020-12-18 20:09:49 +0300

    5.3.1. Динамический список на массиве. [#406042]

---
+++ chapter_005/src/main/java/list/SimpleArrayList.java
@@ -0,0 +1,76 @@
+package list;
+
+import java.util.*;
+
+/**
+ * Class simple ArrayList
+ * @author Aleksei Usov
+ * @since 18/12/2020
+ */
+
+public class SimpleArrayList<T> implements Iterable<T> {
+
+    private Object[] container;
+    private int position = 0;
+    private int modCount = 0;
+
+    public SimpleArrayList() {
+        this.container = new Object[0];
+    }
+
+    /**
+     * method get element for index
+     * @param index of element
+     * @return element
+     */
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        Objects.checkIndex(index, position);
+        return (T) this.container[index];
+    }
+
+    /**
+     * method adding new element to array Object[]
+     * if array is full, method increases its size by 1
+     * @param element adding element
+     */
+
+    public void add(T element) {
+        if (position >= this.container.length) {
+            this.container = Arrays.copyOf(this.container, position + 1);
+        }
+        this.container[position] = element;
+        position++;
+        modCount++;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<>() {
+
+            private int count = 0;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+
+                //checks if the collection has changed since the iterator call
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < position;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) container[count++];
+            }
+        };
+    }
+}
