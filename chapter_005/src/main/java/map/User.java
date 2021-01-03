0feddb3d0248bb867515a43556eb5528487f443a
Aleksei Usov <usalekse@gmail.com>
2021-01-03 15:44:39 +0300

    5.5.4. Переопределить только equals [#406073]

--- chapter_005/src/main/java/map/User.java
+++ chapter_005/src/main/java/map/User.java
@@ -38,8 +38,15 @@ public class User {
 
     /** @noinspection checkstyle:EqualsHashCode*/
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
     }
 
     public static void main(String[] args) {
