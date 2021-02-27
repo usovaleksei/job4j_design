923e8567bc763bcd6d112d2203b0db3f1cdd7f78
Usov Aleksei <usalekse@gmail.com>
2021-02-27 21:06:16 +0300

    6.0.2. FileInputStream [#406100] ver.1

---
+++ chapter_006/src/main/java/inout/EvenNumberFile.java
@@ -0,0 +1,40 @@
+package inout;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Class read file and show even numbers
+ *
+ * @author Alexei Usov
+ * @since 27/02/2021
+ */
+
+public class EvenNumberFile {
+
+    public static void main(String[] args) {
+
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder result = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                result.append((char) read);
+            }
+
+            String[] lines = result.toString().split(System.lineSeparator());
+
+            for (String str : lines) {
+                int rsl = Integer.parseInt(str);
+                if (rsl % 2 != 0) {
+                    System.out.println(str + " - нечетное число");
+                } else {
+                    System.out.println(str + " - четное число");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
