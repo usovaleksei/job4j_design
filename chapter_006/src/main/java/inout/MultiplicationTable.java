49f343f969cfc38f495fea0a5d5b79671ca6d985
Usov Aleksei <usalekse@gmail.com>
2021-02-27 09:50:52 +0300

    6.0.1. FileOutputStream. [#406090] Запись таблицы умножения в файл, ver.2

--- chapter_006/src/main/java/inout/MultiplicationTable.java
+++ chapter_006/src/main/java/inout/MultiplicationTable.java
@@ -1,28 +1,30 @@
 package inout;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
  * class write multiplication table in file
+ *
  * @author Aleksei Usov
- * @since 26/02/2021
+ * @since 27/02/2021
  */
 
 public class MultiplicationTable {
 
     public static void main(String[] args) {
 
-        try (FileOutputStream out = new FileOutputStream("multiplication_table.txt")) {
-            for (int i = 1; i <= 9; i++) {
-                String topString = "\n" + "Умножение на " + i + ": " + "\n";
-                out.write(topString.getBytes());
-                for (int j = 0; j <= 10; j++) {
-                    String result = i + " x " + j + " = " + i * j + "\n";
-                    out.write(result.getBytes());
-                }
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= 9; i++) {
+            result.append(System.lineSeparator()).append("Умножение на ").append(i).append(": ").append(System.lineSeparator());
+            for (int j = 0; j <=10; j++) {
+                result.append(i).append(" x ").append(j).append(" = ").append(i * j).append(System.lineSeparator());
             }
+        }
+
+        try (FileOutputStream out = new FileOutputStream("Multiplication_table.txt")) {
+            out.write(result.toString().getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
