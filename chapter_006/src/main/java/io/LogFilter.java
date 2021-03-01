4f66e817795e603c8e7e080e23cc234b861b286d
Aleksei Usov <usalekse@gmail.com>
2021-03-01 22:24:12 +0300

    6.0.3. BufferedReader. [#406091] Возвращает из файла строки с заданным условием, ver.2

--- chapter_006/src/main/java/io/LogFilter.java
+++ chapter_006/src/main/java/io/LogFilter.java
@@ -3,9 +3,11 @@ package io;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Class show lines from file by condition
+ *
  * @author Alexei Usov
  * @since 27/02/2021
  */
@@ -14,41 +16,31 @@ public class LogFilter {
 
     /**
      * method filtered lines from file by condition
+     *
      * @param file file to analyze
      * @return list of lines where condition is true
      */
 
     public static List<String> filter(String file) {
         List<String> filtered = new ArrayList<>();
-        List<String> lines = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            in.lines().forEach(lines::add);
+            filtered = in.lines().filter(line -> line.contains("404")).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        for (String line : lines) {
-            String[] splited = line.split(" ");
-            if (splited[splited.length - 2].equals("404")) {
-                filtered.add(line);
-            }
-        }
-
         return filtered;
     }
 
     /**
      * method save filtered lines to file
-     * @param log list with filtered lines
+     *
+     * @param log  list with filtered lines
      * @param file where saved lines
      */
 
     public static void save(List<String> log, String file) {
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
-            for (String line : log) {
-                out.write(line);
-                out.write(System.lineSeparator());
-            }
+            log.forEach(out::println);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
