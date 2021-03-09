eb8e5cc6af8215351a4fdb50a62a1dc928c672de
Aleksei Usov <usalekse@gmail.com>
2021-03-09 16:42:27 +0300

    6.1.1. Читаем файл конфигурации [#406096] v.2

--- chapter_006/src/test/java/io/ConfigTest.java
+++ chapter_006/src/test/java/io/ConfigTest.java
@@ -2,6 +2,8 @@ package io;
 
 import org.junit.Test;
 
+import java.util.Objects;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,7 +11,7 @@ public class ConfigTest {
 
     @Test
     public void whenPairWithoutComment() {
-        String path = "./data/app.properties";
+        String path = Objects.requireNonNull(Config.class.getClassLoader().getResource("app.properties")).getFile();
         Config config = new Config(path);
         config.load();
         assertThat(config.value("name"), is("Aleksei Usov"));
@@ -17,7 +19,7 @@ public class ConfigTest {
 
     @Test
     public void whenPairWithComment() {
-        String path = "./data/app_comment.properties";
+        String path = Objects.requireNonNull(Config.class.getClassLoader().getResource("app_comment.properties")).getFile();
         Config config = new Config(path);
         config.load();
         assertThat(config.value("keyOne"), is("valueOne"));
--- src/main/java/ru/job4j/Trigger.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class Trigger {
-
-    public int someLogic() {
-        return 1;
-    }
-}
