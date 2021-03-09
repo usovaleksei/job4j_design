24448179dc01bee2c7c2fcae858769d3b6288081
Aleksei Usov <usalekse@gmail.com>
2021-03-09 23:45:24 +0300

    6.1.3.0. Тестирование IO [#406088] Интеграционные тесты к классу Анализ доступности сервера

--- chapter_006/src/test/java/io/AnalizyTest.java
+++ chapter_006/src/test/java/io/AnalizyTest.java
@@ -1,27 +1,37 @@
 package io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void whenReadThenWriteLog() throws IOException {
         Analizy log = new Analizy();
-        String source = "./data/serverlog.txt";
-        String target = "./data/unavailable.csv";
-        String expected = "10:57:01;10:59:01";
-        String rsl;
-        log.unavailable(source, target);
-        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
-            rsl = reader.readLine();
+        File source = new File("serverlog.txt");
+        File target = new File("unavailable.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01" + System.lineSeparator());
+            out.println("500 10:57:01" + System.lineSeparator());
+            out.println("400 10:58:01" + System.lineSeparator());
+            out.println("200 10:59:01" + System.lineSeparator());
+            out.println("500 11:01:02" + System.lineSeparator());
+            out.println("200 11:02:02");
         }
-        assertThat(expected, is(rsl));
+        log.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target.getAbsolutePath()))) {
+            in.lines().forEach(rsl::append);
+        }
+        String expected = ("10:57:01;10:59:01" + "11:01:02;11:02:02");
+        assertThat(rsl.toString(), is(expected));
     }
 }
