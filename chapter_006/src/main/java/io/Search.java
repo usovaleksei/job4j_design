43424c3e01951befd0e96a7c073eb73707bfd66d
Aleksei Usov <usalekse@gmail.com>
2021-03-10 15:54:20 +0300

    6.1.4.1. Сканирование файловой системы. [#406087] Поиск файлов с заданным расширением

---
+++ chapter_006/src/main/java/io/Search.java
@@ -0,0 +1,39 @@
+package io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * class searching files with condition
+ * @author Aleksei Usov
+ * @since 10/03/2021
+ */
+
+public class Search {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            throw new IllegalStateException("Root folder or etx not set");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
+
+    }
+
+    /**
+     * method searching files with specific extension
+     * @param root root folder
+     * @param ext extension of searching files
+     * @return list of files with ext
+     * @throws IOException io
+     */
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
