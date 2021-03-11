c571384c07d0e479002ad758a67ff8232775ae5a
Aleksei Usov <usalekse@gmail.com>
2021-03-11 23:06:50 +0300

    6.1.4.1. Сканирование файловой системы. [#406087] ver.2

--- chapter_006/src/main/java/io/Search.java
+++ chapter_006/src/main/java/io/Search.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * class searching files with condition
@@ -19,19 +20,19 @@ public class Search {
             throw new IllegalStateException("Root folder or etx not set. Usage java -jar dir.jar ROOT_FOLDER ETX");
         }
         Path start = Paths.get(args[0]);
-        search(start, args[1]).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     /**
      * method searching files with specific extension
      * @param root root folder
-     * @param ext extension of searching files
+     * @param condition of searching files
      * @return list of files with ext
-     * @throws IOException io
+     * @throws IOException if an I/O error has occurred
      */
 
-    public static List<Path> search(Path root, String ext) throws IOException {
-        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
