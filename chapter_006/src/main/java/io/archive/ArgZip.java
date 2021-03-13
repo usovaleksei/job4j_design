0bd3bebf4af384a8feeebdb61eaffd4221693526
Aleksei Usov <usalekse@gmail.com>
2021-03-13 08:35:34 +0300

    6.1.5.2. Архивировать проект [#406098] with comments

--- chapter_006/src/main/java/io/archive/ArgZip.java
+++ chapter_006/src/main/java/io/archive/ArgZip.java
@@ -1,25 +1,48 @@
 package io.archive;
 
+/**
+ * class to working with entry arguments
+ * @author Aleksei Usov
+ * @since 13/03/2021
+ */
+
 public class ArgZip {
 
+    //entry arguments
     private final String[] args;
 
     public ArgZip(String[] args) {
         this.args = args;
     }
 
+    /**
+     * method checks all arguments were transferred or not
+     * @return true if all argument were transferred
+     */
     public boolean valid() {
         return !(directory().isEmpty() || exclude().isEmpty() || output().isEmpty());
     }
 
+    /**
+     * method to get directory name
+     * @return directory name from entry argument
+     */
     public String directory() {
         return this.args[1];
     }
 
+    /**
+     * method to get file parameter for exclusion
+     * @return file parameter from entry argument
+     */
     public String exclude() {
         return this.args[3];
     }
 
+    /**
+     * method to get zip directory name
+     * @return zip directory name from entry argument
+     */
     public String output() {
         return this.args[5];
     }
--- chapter_006/src/main/java/io/archive/Zip.java
+++ chapter_006/src/main/java/io/archive/Zip.java
@@ -9,7 +9,19 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * class packing directory
+ * @author Aleksei Usov
+ * @since 13/03/2021
+ */
+
 public class Zip {
+
+    /**
+     * method packing folder and all files in this folder
+     * @param sources folder to packing
+     * @param target folder to archive
+     */
 
     public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream((new BufferedOutputStream(new FileOutputStream(target))))) {
@@ -21,11 +33,25 @@ public class Zip {
         }
     }
 
+    /**
+     * method packing single file
+     * @param source file name to packing
+     * @param target file name to archive
+     * @throws IOException if an I/O error has occurred
+     */
+
     public void packSingleFile(File source, File target) throws IOException {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.getPath())))) {
             addToZip(zip, source);
             }
     }
+
+    /**
+     * method write file contents to ZipEntry object
+     * @param zip output stream
+     * @param source directory name to packing
+     * @throws IOException if an I/O error has occurred
+     */
 
     private void addToZip(ZipOutputStream zip, File source) throws IOException {
         zip.putNextEntry(new ZipEntry(source.getPath()));
