1228c0da105a713f2b23c5fac5d411b0a27f0df9
Aleksei Usov <usalekse@gmail.com>
2021-03-25 17:19:18 +0300

    6.2.0. Что такое Socket? [#406112]

--- chapter_006/src/main/java/io/sockets/EchoServer.java
+++ chapter_006/src/main/java/io/sockets/EchoServer.java
@@ -7,23 +7,32 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Class emulated server work
+ * @author Aleksei Usov
+ * @since 25/03/2021
+ */
+
 public class EchoServer {
-    public static void main(String[] args) {
+    /**
+     * @noinspection checkstyle:InnerAssignment
+     */
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     String str;
-                   /* while (!(str = in.readLine()).isEmpty()) {
+                    while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                    }*/
+                        if (str.contains("?msg=Bye")) {
+                            server.close();
+                        }
+                    }
                     out.write("HTTP/1.1 200 OK\r\n".getBytes());
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-
 }
