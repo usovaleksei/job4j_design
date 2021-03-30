bc0210461a5d484d8594d51404e8a5eaf748116e
Usov Aleksei <usalekse@gmail.com>
2021-03-30 20:20:41 +0300

    6.2.1. Бот [#406113]

--- chapter_006/src/main/java/io/sockets/EchoServer.java
+++ chapter_006/src/main/java/io/sockets/EchoServer.java
@@ -9,29 +9,36 @@ import java.net.Socket;
 
 /**
  * Class emulated server work
+ *
  * @author Aleksei Usov
  * @since 25/03/2021
  */
 
 public class EchoServer {
-    /**
-     * @noinspection checkstyle:InnerAssignment
-     */
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String msg = "";
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("?msg=Bye")) {
-                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
-                            server.close();
+                        if (str.contains("?msg=")) {
+                            msg = str;
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    if (msg.contains("?msg=Exit")) {
+                        out.write("Bye".getBytes());
+                        server.close();
+                    } else if (msg.contains("?msg=Hello")) {
+                        out.write("Hello, dear friend".getBytes());
+                    } else {
+                        out.write("What".getBytes());
+                    }
                 }
             }
         }
