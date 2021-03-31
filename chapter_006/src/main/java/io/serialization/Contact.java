6838929d3b31b7245ed5f4233e36e894ed0150dc
Usov Aleksei <usalekse@gmail.com>
2021-03-31 23:09:34 +0300

    6.2.4.1. Что такое Сериализация? [#406102]

---
+++ chapter_006/src/main/java/io/serialization/Contact.java
@@ -0,0 +1,54 @@
+package io.serialization;
+
+import java.io.*;
+import java.nio.file.Files;
+
+/**
+ * class demo serialization work
+ * @author Aleksei Usov
+ * @since 31/03/2021
+ */
+
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final int zipCode;
+    private final String phone;
+
+    public Contact(int zipCode, String phone) {
+        this.zipCode = zipCode;
+        this.phone = phone;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "zipCode=" + zipCode +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Contact contact = new Contact(123456, "+7(910)777-77-77");
+        System.out.println("Object before serialization: " + contact);
+
+        File tempFile = Files.createTempFile(null, null).toFile();
+        try (FileOutputStream fos = new FileOutputStream(tempFile);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(contact);
+        }
+
+        try (FileInputStream fis = new FileInputStream(tempFile);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            final Contact contactFromFile = (Contact) ois.readObject();
+            System.out.println("Object after serialization: " + contactFromFile);
+        }
+    }
+}
