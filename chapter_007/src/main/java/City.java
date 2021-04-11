be10ebd10b69c83fb78844b9db4064713bb2ff0b
Usov Aleksei <usalekse@gmail.com>
2021-04-11 14:27:05 +0300

    7.5.0.2. PrepareStatement [#405996]

---
+++ chapter_007/src/main/java/City.java
@@ -0,0 +1,44 @@
+/**
+ * Class data model city
+ * @author Aleksei Usov
+ * @since 10/04/2021
+ */
+
+public class City {
+
+    private int id;
+
+    private String name;
+
+    private int population;
+
+    public City(int id, String name, int population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
