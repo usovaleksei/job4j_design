9e04b76d9a2e316a020241a7448ba1e2fe23ff39
Usov Aleksei <usalekse@gmail.com>
2021-05-24 11:53:46 +0300

    8.1.0. Понятие GC (сборщик мусора) [#6851]

---
+++ chapter_008/src/main/java/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+    private int age;
+    private String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.printf("Removed %d %s%n", age, name);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
